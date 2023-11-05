c6d39cf3cc8118bc50838875c89c9ace9f9db9c2
emily_m <morin.emily@gmail.com>
2023-11-05 17:31:07 -0500

    Add form submit handler

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import NewFoodInput from "./NewFoodInput";
 import "./styles.css";
 
 export default function App() {
+  const [foodsToTry, setFoodsToTry] = useState([]);
+
+  const addFoodItem = (food) => {
+    let foodsArr = [...foodsToTry];
+    foodsArr.push(food);
+    setFoodsToTry(foodsArr);
+  }
+  
   return (
     <div className="App">
       <h1>New Foods to Try</h1>
-      <NewFoodInput />
-      <ul className="food-list"></ul>
+      <NewFoodInput addFoodItem={addFoodItem} />
+      <ul className="food-list">
+        {foodsToTry.map((f, index) => {
+          return <li key={index}>{f}</li>
+        })}
+      </ul>
     </div>
   );
 }
