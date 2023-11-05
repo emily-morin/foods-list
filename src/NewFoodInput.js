195cc53e637551f892b2edfbd3dbfc23e60dfd43
emily_m <morin.emily@gmail.com>
2023-11-05 17:12:30 -0500

    Add input change handler

--- src/NewFoodInput.js
+++ src/NewFoodInput.js
@@ -3,9 +3,13 @@ import React, { useState } from "react";
 const NewFoodInput = () => {
   const [food, setFood] = useState("");
 
+  const updateFood = (e) => {
+    setFood(e.target.value);
+  }
+
   return (
     <form className="food-form">
-      <input type="text" />
+      <input type="text" value={food} onChange={updateFood} />
       <button>Add to the list!</button>
     </form>
   );
