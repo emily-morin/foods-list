e3ea9eee4f3c78b55385e19ed9565f1c3f152182
emily_m <morin.emily@gmail.com>
2023-11-29 21:18:16 -0500

    Add strikethrough click handler and styles

--- src/Food.js
+++ src/Food.js
@@ -1,8 +1,18 @@
-import React from "react";
+import React, {useState} from "react";
 
 const Food = (props) => {
+    const [tried, setTried] = useState(false);
+    
+    const tryFood = (e) => {
+        let newTried = !tried;
+        setTried(newTried);
+    }
+
     return (
-    <li>
+    <li
+        className={tried ? "strike" : ""}
+        onClick={tryFood} 
+    >
         {props.food}
         <span className="delete" onClick={() => props.deleteFood(props.food)}>X</span>
     </li>
